79cf8c5f6492db5a92292f7e560fd2bb3c1b9f8d
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 05:30:06 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -49,3 +49,7 @@ my_hash[:third] = "I'm third!"
  
 puts my_hash
 # > {first: "I'm first!", second: "I'm second!", third: "I'm third!"}
+# bad example
+ 
+my_hash = {first: "I'm first!", second: "I'm second!"}
+my_hash = {first: "I'm first!", second: "I'm second!", third: "I'm third!"}
