d482b6047d153873286c054cb69bb50fecba33ae
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 05:29:58 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -42,3 +42,10 @@ describe "nested hash" do
   end
 end
 
+# good example
+ 
+my_hash = {first: "I'm first!", second: "I'm second!"}
+my_hash[:third] = "I'm third!"
+ 
+puts my_hash
+# > {first: "I'm first!", second: "I'm second!", third: "I'm third!"}
